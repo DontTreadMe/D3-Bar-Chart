08ebc6a1f5f1ed01ea4d62aff577940e3fd73bfc
DontTreadMe <31659942+DontTreadMe@users.noreply.github.com>
2018-08-27 16:15:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,23 +12,36 @@ const USGDP = {
   padding: 40,
   onReady: () => {
     json=JSON.parse(req.responseText);
-    const dataset = json.data;
-    console.log(dataset[0][1]);
+    const dataset = json.data;    
     const rectW = USGDP.w / dataset.length;
-    console.log(dataset[0][1]);
+    console.log(d3.min(dataset, (d) => d[1]) + ", " + d3.max(dataset, (d) => d[1]));
+    
+    const xScale = d3.scaleLinear()
+                     .domain([0, dataset.length])
+                     .range([USGDP.padding, USGDP.w - USGDP.padding]);
+    
+    const hScale = d3.scaleLinear()
+                     .domain([d3.min(dataset, (d) => d[1]), d3.max(dataset, (d) => d[1])])
+                     .range([USGDP.padding, USGDP.h - USGDP.padding]);
+    
+    const yScale = d3.scaleLinear()
+                     .domain([d3.min(dataset, (d) => d[1]), d3.max(dataset, (d) => d[1])])
+                     .range([USGDP.h - USGDP.padding, USGDP.padding]);
+    
+    
     const svg = d3.select(".diagram")
                   .append("svg")
-                  .attr("width", USGDP.w)
-                  .attr("height", USGDP.h);
+                  .attr("width", USGDP.w + 100)
+                  .attr("height", USGDP.h + 60);
     
     svg.selectAll("rect")
        .data(dataset)
        .enter()
        .append("rect")
-       .attr("x", (d, i) => i * rectW + USGDP.padding)
-       .attr("y", (d) => USGDP.h - d[1] - USGDP.padding)
+       .attr("x", (d, i) => i * rectW + 50)
+       .attr("y", (d) => yScale(d[1]))
        .attr("width", rectW)
-       .attr("height", (d) => d[1])
-       .attr("fill", "navy")
+       .attr("height", (d) => hScale(d[1]))
+       .attr("fill", "#A647B2")
   }
 }
